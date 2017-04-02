7a9a74143f229ab7044092ee63091f2abdf90752
RyanCorrell22 <RyanCorrell22@gmail.com>
2017-04-02 14:06:04 +0900

    Added database boot
    created DB

---
+++ fullstack-nanodegree-vm-master/vagrant/catalog/database_setup.py
@@ -0,0 +1,18 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+##End of file##
+
+engine = create_engine(
+    'sqlite:///restaurantmenu.db')
+
+    Base.metadata.create_all(engine)
